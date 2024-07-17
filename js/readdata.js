e5115db948e1d3f73507f2ee81fed93af6cb5fd9
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-17 22:05:21 +0800

    Update readdata.js

--- js/readdata.js
+++ js/readdata.js
@@ -7,7 +7,9 @@ fetch('https://x8ki-letl-twmt.n7.xano.io/api:iGbUspz7/auth/me', {
 })
 .then((response) => response.json())
 .then((data) => {
-  console.log(data); // User data from Xano
+  // Set the innerHTML of the created HTML elements
+  document.getElementById('username-display').innerHTML = data.username;
+  document.getElementById('password-display').innerHTML = data.password;
 })
 .catch((error) => {
   console.error(error);
