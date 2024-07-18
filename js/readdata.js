b846f8237448cd489a56e98c95a299f6a2757275
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-18 11:19:33 +0800

    Update readdata.js

--- js/readdata.js
+++ js/readdata.js
@@ -2,7 +2,7 @@ fetch('https://x8ki-letl-twmt.n7.xano.io/api:iGbUspz7/auth/me', {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + localStorage.getItem('auth') // Use the stored JWT
+    'Authorization': 'Bearer ' + localStorage.getItem('authToken') // Use the stored JWT
   }
 })
 .then((response) => response.json())
