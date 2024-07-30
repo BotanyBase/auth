f20297ce3d0b4369f06958b5c20e71bf6e5eeaa4
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-30 23:00:22 +0800

    Update readdata.js

--- js/readdata.js
+++ js/readdata.js
@@ -8,10 +8,10 @@ fetch('https://x8ki-letl-twmt.n7.xano.io/api:iGbUspz7/auth/me', {
 .then((response) => response.json())
 .then((data) => {
   // Set the innerHTML of the created HTML elements
-  document.getElementById('username-display').innerHTML = data.name;
-  document.getElementById('password-display').innerHTML = data.email;
-  document.getElementById('timestamp').innerHTML = data.created_at; //'Account created at: ' + data.created_at;
-  document.getElementById('role-display').innerHTML = data.massroom_role; //'Account created at: ' + data.created_at;
+  document.getElementById('username-display').innerHTML = 'Your username is: ' + data.name;
+  document.getElementById('password-display').innerHTML = 'Your user email is: ' + data.email;
+  document.getElementById('timestamp').innerHTML = 'Your account was created at: ' + data.created_at; //'Account created at: ' + data.created_at;
+  document.getElementById('role-display').innerHTML = 'Your role is' + data.massroom_role; //'Account created at: ' + data.created_at;
 })
 .catch((error) => {
   console.error(error);
